1d2fa73a93a0a8548cccfa88b7bbd69562414cd7
Santiago Galindo <sp.galindoh@gmail.com>
2021-10-05 10:54:59 -0500

    Typography

--- components/post-body.js
+++ components/post-body.js
@@ -2,33 +2,31 @@ import { StructuredText, Image } from "react-datocms";
 
 export default function PostBody({ content }) {
   return (
-    <div className="max-w-7xl mx-auto">
-      <div className="prose-lg prose-blue">
-        <StructuredText
-          data={content}
-          renderBlock={({ record }) => {
-            if (record.__typename === "ImageBlockRecord") {
-              return <Image data={record.image.responsiveImage} />;
-            }
-            if (record.__typename === "RawHtmlRecord") {
-              return (
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: record.contentRecirculation,
-                  }}
-                />
-              );
-            }
-
+    <div className="prose prose-lg prose-blue max-w-4xl">
+      <StructuredText
+        data={content}
+        renderBlock={({ record }) => {
+          if (record.__typename === "ImageBlockRecord") {
+            return <Image data={record.image.responsiveImage} />;
+          }
+          if (record.__typename === "RawHtmlRecord") {
             return (
-              <>
-                <p>Don't know how to render a block!</p>
-                <pre>{JSON.stringify(record, null, 2)}</pre>
-              </>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: record.contentRecirculation,
+                }}
+              />
             );
-          }}
-        />
-      </div>
+          }
+
+          return (
+            <>
+              <p>Don't know how to render a block!</p>
+              <pre>{JSON.stringify(record, null, 2)}</pre>
+            </>
+          );
+        }}
+      />
     </div>
   );
 }
